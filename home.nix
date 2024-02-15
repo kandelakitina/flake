69a85601e973d9d54f00774c135d960b58e26f6f
boticelli <kandelakitina@gmail.com>
2024-02-15 19:03:30 +0000

    Add packages to home.nix

--- home.nix
+++ home.nix
@@ -11,19 +11,53 @@
 
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
+    # TODO install fish, neovim, tmux, starship
+    # urlview for tmux
+    # taskwarrior
+    # TODO setup fzf
+    # TODO setup starship
+    # TODO setup fish
+    # TODO setup all .config
+    
     # GUI
-    alacritty
+    alacritty # terminal 
     firefox
-    obsidian
+    obsidian  # note taking app
 
-    # Utils
-    # home-manager
+    # # Utils
+    # home-manager    # nix home manager
+    # gcc_multi       # make utils
+    # direnv          # set env variables for folders
+    # entr            # Run arbitrary commands when files change
+    
+    # cli apps
+    bat       # better cat
+    btop      # resourese manager
+    eza       # better ls
+    lazygit   # git client
+    helix     # editor
+    gh        # github authentificator
+    fd        # alternative to find
+    fff       # simple file manager
+    fzf       # fuzzy finder
+    jq        # JSON formatter
+    ripgrep   # better grep
+    tree      # show file tree
+    zoxide    # easy cli files navigator with z
+    zk        # note taking
+    xclip     # system buffer manager
 
-    #cli
-    btop
-    lazygit
-    helix
-    gh
+    # Development and LSPs
+    nodejs
+    deno
+    nodePackages.bash-language-server
+    marksman
+    nodePackages.yaml-language-server
+    nodePackages.vscode-langservers-extracted
+    nodePackages_latest.typescript-language-server
+    # [python3]=python311
+    # [python3.11-pip]=python311Packages.pip
+    # [python3.11-python-lsp-server]=python311Packages.python-lsp-server
   ];
 
   # TODO: add stuff from https://nixos-and-flakes.thiscute.world/nixos-with-flakes/start-using-home-manager
