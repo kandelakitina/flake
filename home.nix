a25a9e28809cff25e6226c0cde6650e131bb4ccb
boticelli <kandelakitina@gmail.com>
2024-02-15 19:49:39 +0000

    add starship and git settings

--- home.nix
+++ home.nix
@@ -68,6 +68,41 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "boticelli";
+    userEmail = "kandelakitina@gmail.com";
+  };
+
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    # settings = {
+    #   add_newline = false;
+    #   aws.disabled = true;
+    #   gcloud.disabled = true;
+    #   line_break.disabled = true;
+    # };
+  };
+
+  # alacritty - a cross-platform, GPU-accelerated terminal emulator
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 16;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
   # programs.emacs = {
   #   enable = true;
   #   extraPackages = epkgs: [
