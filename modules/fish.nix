9ee564e744a15011468a47446aab0d97238a96f9
Mikhail Eremeneko <eremenko86@gmail.com>
2024-02-17 20:03:14 +0300

    Add fish greeting toast

--- modules/fish.nix
+++ modules/fish.nix
@@ -8,6 +8,10 @@
         fff $argv
         set -q XDG_CACHE_HOME; or set XDG_CACHE_HOME $HOME/.cache
         cd (cat $XDG_CACHE_HOME/fff/.fff_d)
-      ''};
+      '';
+    };
+    loginShellInit = ''
+      echo -e "\e[38;5;196m┏(-_-)┛\e[38;5;27m┗(-_-)┓\e[38;5;226m┗(-_-)┛\e[38;5;118m┏(-_-)┓\e[0m"
+    '';
   };
 }
