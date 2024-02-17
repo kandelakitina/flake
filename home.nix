0236a1d8ab5d8468d783fd4773c3b58fb242fa6c
Mikhail Eremeneko <eremenko86@gmail.com>
2024-02-17 17:34:34 +0300

    Refactor home.nix a bit

--- home.nix
+++ home.nix
@@ -1,3 +1,10 @@
+# TODO install fish, neovim, tmux, starship
+# urlview for tmux
+# TODO setup fzf
+# TODO setup starship
+# TODO setup fish
+# TODO setup all .config
+
 { config, pkgs, ... }:
 
 {
@@ -5,32 +12,58 @@
     ./modules/alacritty.nix
   ];
 
+  # Fish
+  programs = {
+    fish.enable = true;
+  };
+
+  # Starship
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      username = {
+        style_user = "blue bold";
+        style_root = "red bold";
+        format = "[$user]($style) ";
+        disabled = false;
+        show_always = true;
+      };
+      hostname = {
+        ssh_only = false;
+        ssh_symbol = "🌐 ";
+        format = "on [$hostname](bold red) ";
+        trim_at = ".local";
+        disabled = false;
+      };
+    };
+  };
+
+  # Shell aliases
   home.shellAliases = {
     g = "git";
     lg = "lazygit";
     f = "fff";
   };
   
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "boticelli";
-  home.homeDirectory = "/home/boticelli";
+  # Git
+  programs.git = {
+    enable = true;
+    userName = "boticelli";
+    userEmail = "kandelakitina@gmail.com";
+  };
 
-  # Turn off versions mismatch errors
-  home.enableNixpkgsReleaseCheck = false;
-
-  # Packages that should be installed to the user profile.
+  # Fonts
+  fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
-    # TODO install fish, neovim, tmux, starship
-    # urlview for tmux
-    # taskwarrior
-    # TODO setup fzf
-    # TODO setup starship
-    # TODO setup fish
-    # TODO setup all .config
+    (nerdfonts.override { fonts = [ "Ubuntu" "UbuntuMono" ]; })
+  ];
+
+  # Packages
+  home.packages = with pkgs; [
     
     # GUI
-    alacritty # terminal 
+    # alacritty # terminal 
     firefox
     obsidian  # note taking app
 
@@ -41,19 +74,19 @@
     # entr            # Run arbitrary commands when files change
     
     # cli apps
-    # bat       # better cat
-    # btop      # resourese manager
+    bat       # better cat
+    btop      # resourese manager
     eza       # better ls
     lazygit   # git client
     helix     # editor
     gh        # github authentificator
     fd        # alternative to find
     fff       # simple file manager
-    # fzf       # fuzzy finder
-    # jq        # JSON formatter
+    fzf       # fuzzy finder
+    jq        # JSON formatter
     ripgrep   # better grep
     tree      # show file tree
-    # zoxide    # easy cli files navigator with z
+    zoxide    # easy cli files navigator with z
     zk        # note taking
     xclip     # system buffer manager
 
@@ -68,60 +101,23 @@
     # [python3]=python311
     # [python3.11-pip]=python311Packages.pip
     # [python3.11-python-lsp-server]=python311Packages.python-lsp-server
-    (nerdfonts.override { fonts = [ "Ubuntu" "UbuntuMono" ]; })
   ];
 
   # TODO: add stuff from https://nixos-and-flakes.thiscute.world/nixos-with-flakes/start-using-home-manager
   
-  # Version of Home Manager with which this config works
-  home.stateVersion = "23.11";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-
   # basic configuration of git, please change to your own
-  programs = {
-    git = {
-      enable = true;
-      userName = "boticelli";
-      userEmail = "kandelakitina@gmail.com";
-    };
+  # programs = {
 
-    # Better `cd`
-    bat.enable = true;
-    # Type `z <pat>` to cd to some directory
-    zoxide.enable = true;
-    # Type `<ctrl> + r` to fuzzy search your shell history
-    fzf.enable = true;
-    jq.enable = true;
-    btop.enable = true;
+  #   # Better `cd`
+  #   bat.enable = true;
+  #   # Type `z <pat>` to cd to some directory
+  #   zoxide.enable = true;
+  #   # Type `<ctrl> + r` to fuzzy search your shell history
+  #   fzf.enable = true;
+  #   jq.enable = true;
+  #   btop.enable = true;
 
-    fish = {
-      enable = true;
-    };
-  
-    starship = {
-      enable = true;
-      enableFishIntegration = true;
-      settings = {
-        username = {
-          style_user = "blue bold";
-          style_root = "red bold";
-          format = "[$user]($style) ";
-          disabled = false;
-          show_always = true;
-        };
-        hostname = {
-          ssh_only = false;
-          ssh_symbol = "🌐 ";
-          format = "on [$hostname](bold red) ";
-          trim_at = ".local";
-          disabled = false;
-        };
-      };
-    }; 
-  };
+  # };
   # # alacritty - a cross-platform, GPU-accelerated terminal emulator
   #   alacritty = {
   #     enable = true;
@@ -138,21 +134,16 @@
   #   };
   # };
 
-  # Fonts
-  fonts.fontconfig.enable = true;
+  # Name
+  home.username = "boticelli";
+  home.homeDirectory = "/home/boticelli";
 
-  # programs.emacs = {
-  #   enable = true;
-  #   extraPackages = epkgs: [
-  #     epkgs.nix-mode
-  #     epkgs.magit
-  #   ];
-  # };
-  #
-  # services.gpg-agent = {
-  #   enable = true;
-  #   defaultCacheTtl = 1800;
-  #   enableSshSupport = true;
-  # };
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 
+  # Turn off versions mismatch errors
+  home.enableNixpkgsReleaseCheck = false;
+  
+  # Version of Home Manager with which this config works
+  home.stateVersion = "23.11";
 }
--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -1,5 +1,3 @@
-# { config, lib, pkgs, ... }:
-
 { config, lib, pkgs, ... }:
 
 {
