fbdce5b980e9ab3c1191a5d375f74564277a346c
boticelli <kandelakitina@gmail.com>
2024-03-03 06:53:59 +0000

    Add user config for NixOS

---
+++ nixos/users/boticelli.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+in {
+  home-manager.users.boticelli = import ../../hosts/${config.networking.hostName}/home.nix;
+
+  # # TODO: add secrets management
+  # sops.secrets.boticelli-password = {
+  #   sopsFile = ./secrets.yaml;
+  #   neededForUsers = true;
+  # };
+
+  #users.mutableUsers = false;
+  users.users.boticelli = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    extraGroups =
+      [
+        "wheel"
+        "video"
+        "audio"
+      ]
+      ++ ifTheyExist [
+        "networkmanager"
+        "libvirtd"
+        "kvm"
+        "docker"
+        "podman"
+        "git"
+        "network"
+        "wireshark"
+        "i2c"
+        "tss"
+        "plugdev"
+      ];
+
+    #hashedPasswordFile = config.sops.secrets.boticelli-password.path;
+    packages = [pkgs.home-manager];
+  };
+
+  programs.fish.enable = true;
+}
